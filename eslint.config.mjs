cd0290d20cd1c729200394ea6071f43cc73e94a7
Doug Parker <dgp1130@users.noreply.github.com>
2026-09-04 22:05:43 -0700

    Ignore nested jj workspaces in ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist/**", "node_modules/**", "eslint.config.mjs"] },
+  { ignores: ["dist/**", "node_modules/**", "eslint.config.mjs", ".workspaces/**"] },
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
